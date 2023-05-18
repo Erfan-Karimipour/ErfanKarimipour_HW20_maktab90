629b3a85effc639526a396ff6d63a5dc66f2d7ab
Erfan-Karimipour <programming1402@gmail.com>
2023-05-18 08:29:21 +0330

    Improved

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,25 +1,6 @@
 import React, { ReactNode, createContext, useContext, useState } from 'react'
 
 let Context = createContext({});
-
-
-// function setState(state, action){
-    
-    //     switch (action.type){
-        //         case 'nextPage':
-        //             return {
-//                 ...state,
-//                 step: state.step ++
-//             }
-//             break;
-//         case 'previousPage':
-//             return {
-    //                 ...state,
-    //                 step: state.step --
-    //             }
-    //             break;
-    //     }
-    // }
     
     export function Providers ({children}: {children: ReactNode}){
 
